c760c01e2a9af0c8de12dbb3486a1a9038f5476d
MrPink <will.pink@gmail.com>
2016-12-19 11:16:59 +0100

    removing output vars and elb module

--- aws/cluster/main.tf
+++ aws/cluster/main.tf
@@ -9,7 +9,7 @@ module "vpc" {
   private_subnets     = "10.0.1.0/24,10.0.2.0/24"
   public_subnets      = "10.0.101.0/24,10.0.102.0/24"
   bastion_instance_id = "${aws_instance.bastion.id}"
-  azs                 = "${var.availability_zones}"
+  availability_zones  = "${var.availability_zones}"
 }
 
 # ssh keypair for instances
@@ -17,13 +17,9 @@ module "aws-ssh" {
   source = "../modules/ssh"
   public_key_file = "${var.public_key_file}"
   private_key_file = "${var.private_key_file}"
-  region = "${var.region}"
-  cluster_name = "${var.cluster_name}"
-  default_instance_user  = "${var.default_instance_user}"
-  master0_ip = "${aws_instance.master.0.private_ip}"
-  master1_ip = "${aws_instance.master.1.private_ip}"
-  worker0_ip = "${aws_instance.worker.0.private_ip}"
 }
+
+module "iam" { source = "../modules/iam" }
 
 # security group to allow all traffic in and out of the instances in the VPC
 module "sg-default" {
@@ -37,14 +33,6 @@ module "route53" {
   vpc_id = "${module.vpc.vpc_id}"
   cluster_name = "${var.cluster_name}"
   environment = "${var.environment}"
-}
-
-module "elb" {
- source = "../modules/elb"
-
- security_groups = "${module.sg-default.security_group_id}"
- instances       = "${join(",", aws_instance.worker.*.id)}"
- subnets         = "${module.vpc.public_subnets}"
 }
 
 # Generate an etcd URL for the cluster
@@ -63,15 +51,6 @@ resource "template_file" "etcd_discovery_url" {
 output "bastion.ip" {
   value = "${aws_eip.bastion.public_ip}"
 }
-output "master_ips" {
-  value = "${join(",", aws_instance.master.*.private_ip)}"
-}
-output "worker_ips" {
-  value = "${join(",", aws_instance.worker.*.private_ip)}"
-}
 output "vpc_cidr_block_ip" {
  value = "${module.vpc.vpc_cidr_block}"
-}
-output "elb.hostname" {
-  value = "${module.elb.elb_dns_name}"
 }
